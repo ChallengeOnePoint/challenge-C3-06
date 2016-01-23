84b3cd4496bf99c2f8f4723d011c0d05272d683b
Baptiste Jamin <baptistejamin@gmail.com>
2016-01-23 17:20:04 +0100

    fixes

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,11 +8,15 @@
  * Controller of the todoApp
  */
 angular.module('todoApp')
-  .controller('MainCtrl', function (Api) {
-    //$scope.todos = Api.todos;
+  .controller('MainCtrl', function ($scope, Api) {
+    $scope.todos = Api.todos;
 
     //Object Todo {}
-    //$scope.addTodo = Api.addTodo;
-    //$scope.updateTodo = Api.updateTodo;
-    //$scope.removeTodo = Api.removeTodo;
+    $scope.addTodo = Api.addTodo;
+    $scope.updateTodo = Api.updateTodo;
+    $scope.removeTodo = Api.removeTodo;
+
+    $scope.addTodo({
+      title : "test"
+    });
   });
